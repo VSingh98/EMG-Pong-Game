95ea0dec4734b973d475b053026633e18eb46f50
Vansh <vanshchaudhary@gmail.com>
2016-03-03 17:25:35 -0800

    backbone for system

---
+++ calibrate.py
@@ -0,0 +1,79 @@
+import serial
+import change_mode
+
+# declare global variables used for signal processing
+b_low, a_low = scipy.signal.butter(3, .5, 'low', analog=False)
+b_high, a_high = scipy.signal.butter(3, 0.1, 'high', analog=False)
+box = scipy.signal.boxcar(50)
+	
+
+'''
+Acquires calibration data for creating the normalization factor and the 
+threshold value
+'''
+def calibrate(ser):
+	
+	leftArm, rightArm = ( list(),  list() )
+	
+	while True:
+		try:
+			lines = ser.readline()[:-1] # get rid of newline character
+			
+			# looks like we read in an empty string
+			if len(lines) == 0:
+				continue
+
+			if lines[0] == 's'		# if ilne starts with s, we are no longer in
+				raise ChangeMode 	# calibration mode
+			
+			values = lines.split(' ')
+
+			if values[0].isalpha():
+				values = values[1:]
+
+			leftArm.append(int(values[0]))
+			rightArm.append(int(values[1]))
+		
+		except ValueError:
+			write(leftArm, rightArm)
+			raise ChangeMode('we are officially in game mode')		
+
+		except KeyboardInterrupt:
+			write(leftArm, rightArm)
+
+'''
+This will write the max values for the leftArm and rightArm lists after going
+through EMG filtering, rectifying, and smoothing to the lnorm
+and rnorm text files.
+'''
+def write(leftArm, rightArm):
+	
+	leftArm = np.array(leftArm, dtype=float)
+	rightArm = np.array(rightArm, dtype=float)
+
+	leftArm, rightArm = ( scaleToEmg(leftArm), scaleToEmg(rightArm) )
+
+	leftArm, rightArm = ( powerSmooth(leftArm), powerSmooth(rightArm) )
+	
+	with lfile as open("lnorm", 'w'):
+		lfile.write(leftArm.amax())
+
+	with rfile as open("rnorm", 'w'):
+		rfile.write(rightArm.amax())
+
+'''
+This scales an ADC_sampleValue into an EMG voltage
+'''
+def scaleToEmg(ADC_sampleValue):
+    return (ADC_sampleValue * 5.0/1023 - 1.5)/3600
+
+'''
+This performs filtering, rectifying, and smoothing on the paramter
+'''
+def powerSmooth(data)
+	LowPassData = scipy.signal.lfilter(b_low, a_low, data)
+	filteredData = scipy.signal.lfilter(b_high, a_high, LowPassData)
+	rectifiedData = np.absolute(filteredData)
+	smoothData = scipy.signal.lfilter(box, 1, rectifiedData)
+
+	return smoothData
--- mapping.py
+++ mapping.py
@@ -9,34 +9,30 @@ sending a UDP packet corresponding to position to the paddle position
 class Mapper(object):
     
     IP_ADDR = '127.0.0.1' # IP Address to send UDP packet
-    PORTNUM = 0 # Socket endpoint, where to communicate 
+    PORTNUM = 5005 # Socket endpoint, where to communicate 
 
     ANALOG_VAL_THRESHOLD = 0.00002  # threshold for determining muscle flexion
     DELTA = .001    # amuont by which we update position with each
                     # changePosition call
 
-    FAST_SEQUENCE_THRESHOLD = .4    # time in seconds between two arm flexions
-                                    # to do teleporation
-
     UPPER_LIM = 1   # Upper limit of position in game
     LOWER_LIM = 0   # Lower limit of position in game
     MIDDLE = 0.5    # Middle position in game
-    AXIS_SCALAR = 10    # Scalar by which to multiply normalized limits to send
+    AXIS_SCALAR = 1    # Scalar by which to multiply normalized limits to send
                         # in UDP packet to game
 
     position = MIDDLE   # we initialize position to the middle
     
-    times = [-1, -1]    # this will hold the times of the last two calls to
-                        # changePosition
-
     vector = 0  # Will it go up (1), down(-1), or stay (0)
-
-    armVectors = 0  # This is used to determine where the Paddle will teleport to
-                    # if two calls were made within the FAST_SEQUENCE_THRESHOLD
 
     sock = socket.socket(socket.AF_INIT, socket.SOCK_DGRAM) # create a socket
     
     sock.connect((IPADDR, PORTNUM)) # connect socket to server
+
+    LEFT_NORMALIZATION_FACTOR = int(open("lnorm", 'r').readline()[-1:])
+
+    RIGHT_NORMALIZATION_FACTOR = int(open("rnorm", 'r').readline()[-1:])
+
 
     '''
     Purpose: This is method is called by signal filtering script to map the EMG
@@ -46,44 +42,14 @@ class Mapper(object):
     @staticmethod
     def changePosition(leftVal, rightVal, time):
 
-        # determine movement vector from input values
-        if (leftVal > ANALOG_VAL_THRESHOLD and rightVal > \
-                ANALOG_VAL_THRESHOLD):
+        vector = leftVal/LEFT_NORMALIZATION_FACTOR - \
+            rightVal/RIGHT_NORMALIZATION_FACTOR
+
+        if (vector < ANALOG_VAL_THRESHOLD):
             vector = 0
-        elif (leftVal > ANALOG_VAL_THRESHOLD):
-            vector = 1
-        elif (rightVal > ANALOG_VAL_THRESHOLD):
-            vector = -1
-        else:
-            vector = 0
-        
-        # update time values with new time at which this method was called to
-        # move the paddle
-        if (vector != 0):
-            times[1] = times[0]
-            times[0] = time
-        
-        # update armVectors sum
-        armVectors += vector
 
-        # if this method is called back to back to change the paddle within the
-        # FAST_SEQUENCE_THRESHOLD, we perform a teleportation.
-        if (times[0] - times[1] < FAST_SEQUENCE_THRESHOLD):
 
-            if (armVectors == 0): # if right->left or left->right, go to middle
-                position == MIDDLE
-            elif (armVectors == -2): # if right->right, go to bottom
-                position == LOWER_LIM
-            else:                    # if left->left, go to top
-                position == UPPER_LIM
-
-        # if not performing teleportation, move position in vector's direction
-        else:
-            position = position + vector * DELTA
-
-        # if armsVector reaches two, reset back to zero
-        if (armsVector == -2 or armsVector == 2):
-            armsVector = 0
+        position = position + vector * DELTA
 
         # send UPD packet of position to game
         sendUDPPacket()
@@ -101,5 +67,8 @@ class Mapper(object):
         elif position < -1:
             position = -1 
         
-        # send hex decoded integer to Pong server
-        sock.sendTo( hex(position * AXIS_SCALAR) )
+        # send str Pong server
+        sock.sendTo( str(position * AXIS_SCALAR) )
+    
+
+
